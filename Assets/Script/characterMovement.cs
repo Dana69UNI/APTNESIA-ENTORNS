27e2e104c90be698ca2ccabba59cc85341f781e0
Dana69UNI <126713261+Dana69UNI@users.noreply.github.com>
2024-11-19 12:59:05 +0100

    Movimiento Basico Arreglo

--- Assets/Script/characterMovement.cs
+++ Assets/Script/characterMovement.cs
@@ -7,25 +7,41 @@ using UnityEngine.InputSystem;
 
 public class characterMovement : MonoBehaviour
 {
-    public Rigidbody rb; //Coge el rigidbody del personaje
-    private float moveSpeed = 5.0f; //movimiento velocidad
+    private Rigidbody rb; //Coge el rigidbody del personaje
+    private float moveSpeed = 20.0f; //la velocidad
+    private float objDrag = 5f; //drag para que el rb no se resbale
     public InputActionReference move; //VER INPUTS, lo necesitamos para que lea los valores del WASD o del Stick del mando para mover al personaje
-    private Vector2 _moveDirection; //el vector 2 donde guardamos los valores
+    private Vector2 inputDirection; //el vector 2 donde guardamos los valores
+    Vector3 _moveDirection;
 
     void Start()
     {
-       
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.drag = objDrag;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _moveDirection = move.action.ReadValue<Vector2>();
+
+        inputManage();
+    }
+
+   void FixedUpdate()
+    {
         OnMove();
     }
 
+    private void inputManage()
+    {
+       inputDirection = move.action.ReadValue<Vector2>();
+        _moveDirection = transform.forward * inputDirection.y + transform.right * inputDirection.x; //Transform.forward y right para que el movimiento sea acorde a los ejes del personaje. 
+    }
     private void OnMove()
     {
-        rb.velocity = new Vector3(_moveDirection.x * moveSpeed, rb.velocity.y, _moveDirection.y * moveSpeed);
+     
+        rb.AddForce(_moveDirection.normalized * moveSpeed, ForceMode.Acceleration); //en vez de usar rb.velocity como la ultima vez, usamos addforce, solo porque supuestamente lo otro puede llevar a problemas
+    //el .normalized sirve para que la velocidad en diagonal sea la misma que para cualquier lado, (normaliza el vector vaya).
     }
 }

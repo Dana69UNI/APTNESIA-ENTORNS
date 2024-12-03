971d76dfd38a80a98e47bb925897d403706e1be7
Marcos2611 <155254634+Marcos2611@users.noreply.github.com>
2024-12-03 20:12:58 +0100

    papel_acertijo agregado y configurado tambien

--- Assets/Script/PaperInteraction.cs
+++ Assets/Script/PaperInteraction.cs
@@ -1,59 +1,3 @@
-//using UnityEngine;
-
-//public class PaperInteraction : MonoBehaviour
-//{
-//    private bool isPlayerNear = false;  // Variable para detectar proximidad del jugador
-//    public GameObject paperModel;      // Objeto del papel en la escena
-//    public GameObject paperUI;         // UI para mostrar el papel ampliado
-
-//    void Start()
-//    {
-//        if (paperUI != null)
-//        {
-//            paperUI.SetActive(false); // Oculta el UI al inicio
-//        }
-//    }
-
-//    void Update()
-//    {
-//        // Detectar si el jugador presiona "E" cuando está cerca
-//        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
-//        {
-//            ShowPaper();
-//        }
-//    }
-
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        if (other.CompareTag("Player")) // Asegurarse de que el objeto tenga el tag "Player"
-//        {
-//            isPlayerNear = true;
-//            Debug.Log("Presiona E para recoger el papel");
-//        }
-//    }
-
-//    private void OnTriggerExit(Collider other)
-//    {
-//        if (other.CompareTag("Player"))
-//        {
-//            isPlayerNear = false;
-//        }
-//    }
-
-//    void ShowPaper()
-//    {
-//        if (paperModel != null)
-//        {
-//            paperModel.SetActive(false); // Oculta el papel en la escena
-//        }
-//        if (paperUI != null)
-//        {
-//            paperUI.SetActive(true);     // Muestra el UI del papel ampliado
-//        }
-//        Debug.Log("Mostrando el papel en la UI");
-//    }
-//}
-
 using UnityEngine;
 
 public class PaperInteraction : MonoBehaviour
@@ -108,3 +52,4 @@ public class PaperInteraction : MonoBehaviour
         }
     }
 }
+

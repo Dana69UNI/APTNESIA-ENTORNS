2091e8f5c8bc0f4727f1989dcb9eba0e8ca9b3a0
Dana69UNI <126713261+Dana69UNI@users.noreply.github.com>
2024-12-04 11:49:18 +0100

    Rework del ObjInteraction

--- Assets/Script/objInteraction.cs
+++ Assets/Script/objInteraction.cs
@@ -8,65 +8,186 @@ using UnityEngine.InputSystem;
 
 public class objInteraction : MonoBehaviour
 {
-    
+
     public InputActionReference Grab;
     public InputActionReference Throw;
     public Transform intChar;
-    private float forceStrength = 150f;
-    //private float dragStrength = 28f;
-    //private float aDragStrength = 20f;
+    public Rigidbody _emptyRb;
     private Rigidbody rb;
+    [SerializeField] private float grabSpring = 100f;  // Adjust grab strength values / SpringJoint values
+    [SerializeField] private float grabDamper = 4.5f; // Adjust grab strength values / SpringJoint values
+    [SerializeField] private float throwForce = 11.0f;
     bool grabbable = false;
+    private bool isGrabbed;
+    
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        _emptyRb.isKinematic = true;
         interactiveSystem.interNotFound += NoGrab;
+        
     }
     private void FixedUpdate()
+    {
+      
+
+
+    }
+
+    private void Update()
     {
         if (grabbable)
         {
             if (Grab.action.IsPressed())
             {
-
-                //Vector3 direction = intChar.position - transform.position;
-                //rb.drag = dragStrength;
-                //rb.angularDrag = aDragStrength;
-                //rb.useGravity = false;
-                //rb.AddForce(direction.normalized * forceStrength * Time.deltaTime, ForceMode.VelocityChange);
-                gameObject.transform.position = intChar.transform.position;
-
-                if(Throw.action.IsPressed())
+                if(rb.velocity.magnitude < 0.8f)
                 {
-                    NoGrab();
-                    rb.AddForce(intChar.forward * forceStrength * Time.deltaTime, ForceMode.VelocityChange);
-                    NoGrab();
+                    isGrabbed = true;
+                    ApplySpringConstraint();
+                    rb.freezeRotation = true;
                 }
+                
             }
             else
             {
-                //rb.drag = 0f;
-                //rb.angularDrag = 0f;
-                //rb.useGravity = true;
+                ResetShooting();
+            }
+
+        }
+        if(isGrabbed)
+        {
+            if (Throw.action.triggered)
+            {
+                ThrowObject();
             }
         }
-        else
-        {
-            //rb.drag = 0f;
-            //rb.angularDrag = 0f;
-            //rb.useGravity = true;
-            
-        }
-
     }
     private void CanGrab()
     {
-       grabbable = true;
+        grabbable = true;
     }
     private void NoGrab()
     {
         grabbable = false;
     }
-    
+
+    private void ApplySpringConstraint()
+    {
+
+        if (_emptyRb && rb)
+        {
+            SpringJoint spring = rb.gameObject.GetComponent<SpringJoint>();
+            if (!spring)
+            {
+                spring = rb.gameObject.AddComponent<SpringJoint>();
+                spring.autoConfigureConnectedAnchor = false;
+                spring.connectedBody = _emptyRb;
+                spring.connectedAnchor = Vector3.zero;
+                spring.spring = grabSpring;
+                spring.damper = grabDamper;
+
+                spring.massScale = 1f;
+                spring.minDistance = 0.01f;
+                spring.maxDistance = 0.2f;
+
+            }
+        }
+    }
+
+    private void ThrowObject()
+    {
+        ResetShooting();
+        Vector3 throwDirection = intChar.transform.forward;
+        rb.AddForce(throwDirection * throwForce, ForceMode.Impulse);
+        
+
+    }
+
+    private void ResetShooting()
+    {
+        isGrabbed = false;
+        rb.freezeRotation = false;
+        if (rb)
+        {
+
+            Destroy(rb.GetComponent<SpringJoint>());
+        }
+
+    }
 }
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.VisualScripting;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
+//using UnityEngine.InputSystem;
+
+
+//public class objInteraction : MonoBehaviour
+//{
+
+//    public InputActionReference Grab;
+//    public InputActionReference Throw;
+//    public Transform intChar;
+//    private float forceStrength = 150f;
+//    //private float dragStrength = 28f;
+//    //private float aDragStrength = 20f;
+//    private Rigidbody rb;
+//    bool grabbable = false;
+
+//    private void Start()
+//    {
+//        rb = GetComponent<Rigidbody>();
+//        interactiveSystem.interNotFound += NoGrab;
+//    }
+//    private void FixedUpdate()
+//    {
+//        if (grabbable)
+//        {
+//            if (Grab.action.IsPressed())
+//            {
+
+//                //Vector3 direction = intChar.position - transform.position;
+//                //rb.drag = dragStrength;
+//                //rb.angularDrag = aDragStrength;
+//                //rb.useGravity = false;
+//                //rb.AddForce(direction.normalized * forceStrength * Time.deltaTime, ForceMode.VelocityChange);
+//                gameObject.transform.position = intChar.transform.position;
+
+//                if(Throw.action.IsPressed())
+//                {
+//                    NoGrab();
+//                    rb.AddForce(intChar.forward * forceStrength * Time.deltaTime, ForceMode.VelocityChange);
+//                    NoGrab();
+//                }
+//            }
+//            else
+//            {
+//                //rb.drag = 0f;
+//                //rb.angularDrag = 0f;
+//                //rb.useGravity = true;
+//            }
+//        }
+//        else
+//        {
+//            //rb.drag = 0f;
+//            //rb.angularDrag = 0f;
+//            //rb.useGravity = true;
+
+//        }
+
+//    }
+//    private void CanGrab()
+//    {
+//       grabbable = true;
+//    }
+//    private void NoGrab()
+//    {
+//        grabbable = false;
+//    }
+
+//}
